a5c8f3c9c06e3985867d37c3505638061e8e064b
samedamci <samedamci@disroot.org>
2021-08-30 17:51:05 +0200

    Change flake8 installation method

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.7-slim
 
+RUN python -m pip install flake8
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/sh -l
 
-install() {
-    python -m pip install flake8
-}
-
-run() {
-    python -m flake8 ${1} "${2}"
-}
-
-install
-run "${@}"
+python -m flake8 ${1} "${2}"
