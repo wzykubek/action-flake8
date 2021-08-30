da37570b7fca12f036f3aca3efeac010205be062
samedamci <samedamci@disroot.org>
2021-08-30 16:55:21 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python-3.6:slim
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -l
+
+install() {
+    python -m pip install flake8
+}
+
+run() {
+    python -m flake8 ${1} "${2}"
+}
